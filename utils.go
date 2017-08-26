f0fe0831bdb791cac1870d5b83e86b41fbf8dede
Jaime Andrade <jaimeandrade454@gmail.com>
2017-08-26 07:51:32 -0500

    save

--- utils.go
+++ utils.go
@@ -3,12 +3,13 @@ package main
 const user = "bfc6a71ffb843c"
 const pass = "6406db85"
 
-//const url = "us-cdbr-iron-east-05.cleardb.net"
-const url = "127.0.0.1"
-const port = "3306"
+const url = "us-cdbr-iron-east-05.cleardb.net"
+
+//const url = "127.0.0.1"
+const portdb = "3306"
 const namedb = "heroku_38cbea6230473ea"
 
-var linkDataBase = user + ":" + pass + "@tcp(" + url + ":" + port + ")/" + namedb
+var linkDataBase = user + ":" + pass + "@tcp(" + url + ":" + portdb + ")/" + namedb
 
 //var linkDataBase = "admin:12345678@tcp(spacecongame.cffsdiafpjwv.us-east-2.rds.amazonaws.com:3306)/spacecondb"
 var typeDataBase = "mysql"
